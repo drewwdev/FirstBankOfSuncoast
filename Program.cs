234b573f9879c88497be20605de78674dd1bc5b7
Drew Wilson <jankyy93@gmail.com>
2021-08-05 14:37:48 -0400

    basic while loop with some if statements for the menu

--- Program.cs
+++ Program.cs
@@ -1,12 +1,54 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FirstBankOfSuncoast
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Transaction
     {
-      Console.WriteLine("Welcome to C#");
+        public void Withdraw()
+        {
+
+        }
+        public void Deposit()
+        {
+
+        }
+
     }
-  }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to C#");
+
+            var transactionList = new List<Transaction>();
+
+            var keepGoing = true;
+
+            while (keepGoing)
+            {
+                Console.WriteLine("Would you like to:\n(W)ithdraw\n(D)eposit\n(Q)uit\n:");
+                var menuOption = Console.ReadLine().ToUpper();
+
+                if (menuOption == "W")
+                {
+                    Console.WriteLine("Which account would you like to withdraw from:\n(C)hecking\n(S)avings\n:");
+                }
+                else
+                if (menuOption == "D")
+                {
+                    Console.WriteLine("Which account would you like to deposit to:\n(C)hecking\n(S)avings\n:");
+                }
+                else
+                if (menuOption == "Q")
+                {
+                    keepGoing = false;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid selection");
+                }
+            }
+        }
+    }
 }
